d2a39e57c28986cb064259d566ef3e6f2da1ca74
Abdulwahab Isam <abdoi94.iq@gmail.com>
2024-02-25 18:59:23 +0300

    pipa: Electrify
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (c) 2014 The Android Open-Source Project
+# Copyright (c) 2024 StatiXOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/statix_pipa.mk
+
+COMMON_LUNCH_CHOICES := \
+    statix_pipa-user 	 \
+	statix_pipa-userdeubg  \
+	statix_pipa-eng

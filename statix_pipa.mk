0b6c61378f0a393bf762dd04271c1867407162b7
Abdulwahab Isam <abdoi94.iq@gmail.com>
2024-03-25 18:30:17 +0300

    pipa: Include GSM
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- statix_pipa.mk
+++ statix_pipa.mk
@@ -10,6 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit some common statix stuff.
 $(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
 
 # Inherit from pipa device
 $(call inherit-product, device/xiaomi/pipa/device.mk)
@@ -21,7 +22,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Pad 6
 
 PRODUCT_CHARACTERISTICS := tablet
-
+INCLUDE_PIXEL_LAUNCHER := true
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Xiaomi/pipa/pipa:13/RKQ1.211001.001/V14.0.7.0.TMZCNXM:user/release-keys

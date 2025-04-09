10774491b2386096df9ebf28f8d4aac966942dd4
Abdulwahab Isam <abdoi94.iq@gmail.com>
2025-04-09 20:23:33 +0000

    pipa: lets do pixelage
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- pixelage_pipa.mk
+++ pixelage_pipa.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/pixelage/config/common_full_tablet_wifionly.mk)
 
 # Inherit from pipa device
 $(call inherit-product, device/xiaomi/pipa/device.mk)
@@ -17,7 +17,7 @@ $(call inherit-product, device/xiaomi/pipa/device.mk)
 # Inherit keys
 $(call inherit-product, vendor/lineage-priv/keys/keys.mk)
 
-PRODUCT_NAME := lineage_pipa
+PRODUCT_NAME := pixelage_pipa
 PRODUCT_DEVICE := pipa
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
@@ -28,18 +28,13 @@ TARGET_SUPPORTS_QUICK_TAP := false
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Axion specific
-AXION_CAMERA_REAR_INFO := 13
-AXION_CAMERA_FRONT_INFO := 8
-AXION_CPU_SMALL_CORES := 0,1,2,3
-AXION_CPU_BIG_CORES := 4,5,6,7
-AXION_MAINTAINER := Abdulwahab_(ai94iq)
-AXION_PROCESSOR := Qualcomm_Snapdragon_870
-TARGET_PREBUILT_BCR := false
-PRODUCT_NO_CAMERA := false
-TARGET_INCLUDE_MATLOG := true
-TARGET_INCLUDES_LOS_PREBUILTS := true
-TARGET_OTA_ENABLED := false
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+# Pixelage
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=Xiaomi/pipa_global/pipa:13/RKQ1.211001.001/V816.0.7.0.UMZMIXM:user/release-keys
